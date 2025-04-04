250bf5712e3c1babd967d510a24098d20b8caaf5
Ghanshyam Dutt <ghanshyamdutt1407@gmail.com>
2025-04-05 01:31:04 +0530

    added a basic no post found.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import { FoodPostsTimeline, NewFoodPost, FoodPost } from "@/components";
 
 export default function Blog() {
-  const { createPost, foodItems } = useStore();
+  const { createPost, foodItems, toggleCreatePost } = useStore();
 
   useEffect(() => {
     document.title = `Food App`;
@@ -19,10 +19,25 @@ export default function Blog() {
 
         <div id="food-posts">
           {foodItems.map((fp, key) => (
-            <Link href={`/blog/${fp.id}`} key={`food_item_${key}`}>
-              <FoodPost item={fp} />
-            </Link>
+            <FoodPost item={fp} />
           ))}
+          {foodItems.length === 0 && (
+            <div className="bg-gradient-to-br from-amber-400 to-amber-500 rounded-xl mt-26 h-40  w-full flex flex-col items-center justify-center">
+              <h1 className="text-3xl font-medium text-white">
+                No New Posts Found.
+              </h1>
+              <p className="text-lg font-extralight mt-4">
+                Add a{" "}
+                <button
+                  onClick={toggleCreatePost}
+                  type="button"
+                  className="bg-white px-2 rounded-md cursor-pointer"
+                >
+                  New Post
+                </button>
+              </p>
+            </div>
+          )}
         </div>
       </div>
       <div className="w-1/5 h-full py-1 sticky top-0">
--- src/components/FoodItem.tsx
+++ src/components/FoodItem.tsx
@@ -3,65 +3,70 @@ import moment from "moment";
 import Image from "next/image";
 import { UserDetails } from "./UserDetails";
 import { FoodPreference } from "./FoodPreference";
+import Link from "next/link";
 
 export const FoodPost: React.FC<{ item: FoodItem }> = ({ item }) => {
   return (
-    <div
-      className={`w-full border-1 border-gray-200 mb-4 rounded-2xl overflow-hidden cursor-pointer hover:shadow-md transition-all`}
-    >
-      <div className="flex">
-        <div
-          className={`${
-            !item.imageUrl ? "w-full" : "w-1/2"
-          } p-3 flex flex-col justify-between`}
-        >
-          <div className="flex items-center gap-2">
-            <FoodPreference
-              foodPreference={item.food_preference}
-              textVisibility="hidden"
-            />
-            <h3 className="text-lg font-extrabold">{item.title}</h3>
-          </div>
+    <Link href={`/blog/${item.id}`} key={`food_item_${item.id}`}>
+      <div
+        className={`w-full border-1 border-gray-200 mb-4 rounded-2xl overflow-hidden cursor-pointer hover:shadow-md transition-all`}
+      >
+        <div className="flex">
           <div
-            className={`w-full text-md ${
-              !item.imageUrl
-                ? "bg-slate-100 h-30 my-4 rounded-lg flex flex-col items-center justify-center text-xl font-medium"
-                : ""
-            }`}
+            className={`${
+              !item.imageUrl ? "w-full" : "w-1/2"
+            } p-3 flex flex-col justify-between`}
           >
-            <p>{item.description}</p>
+            <div className="flex items-center gap-2">
+              <FoodPreference
+                foodPreference={item.food_preference}
+                textVisibility="hidden"
+              />
+              <h3 className="text-lg font-extrabold">{item.title}</h3>
+            </div>
+            <div
+              className={`w-full text-md ${
+                !item.imageUrl
+                  ? "bg-slate-100 h-30 my-4 rounded-lg flex flex-col items-center justify-center text-xl font-medium"
+                  : ""
+              }`}
+            >
+              <p>{item.description}</p>
+            </div>
+            <div>
+              <div className="flex items-center gap-1 text-gray-500">
+                <small>
+                  {moment(item.datePosted).format("ddd, D MMM YYYY")}
+                </small>
+                <div className="w-1 h-1 rounded-full bg-gray-800"></div>
+                <UserDetails
+                  profile={{
+                    profile_pic: `https://randomuser.me/api/portraits/men/1.jpg`,
+                    username: item.postedBy,
+                  }}
+                  textClasses="font-normal text-sm text-slate-800"
+                  classes="w-5 h-5"
+                />
+              </div>
+              <ul className="flex gap-2">
+                <li className="text-sm">{item.likes} likes</li>
+                <li className="text-sm">{item.comments} comments</li>
+              </ul>
+            </div>
           </div>
-          <div>
-            <div className="flex items-center gap-1 text-gray-500">
-              <small>{moment(item.datePosted).format("ddd, D MMM YYYY")}</small>
-              <div className="w-1 h-1 rounded-full bg-gray-800"></div>
-              <UserDetails
-                profile={{
-                  profile_pic: `https://randomuser.me/api/portraits/men/1.jpg`,
-                  username: item.postedBy,
-                }}
-                textClasses="font-normal text-sm text-slate-800"
-                classes="w-5 h-5"
+          {item.imageUrl && (
+            <div className="w-1/2 h-50 relative">
+              <Image
+                fill
+                sizes="100vw 100vh"
+                src={`/upload/${item.imageUrl}`}
+                alt={item.title || "Image"}
+                style={{ objectFit: "cover" }}
               />
             </div>
-            <ul className="flex gap-2">
-              <li className="text-sm">{item.likes} likes</li>
-              <li className="text-sm">{item.comments} comments</li>
-            </ul>
-          </div>
+          )}
         </div>
-        {item.imageUrl && (
-          <div className="w-1/2 h-50 relative">
-            <Image
-              fill
-              sizes="100vw 100vh"
-              src={`/upload/${item.imageUrl}`}
-              alt={item.title || "Image"}
-              style={{ objectFit: "cover" }}
-            />
-          </div>
-        )}
       </div>
-    </div>
+    </Link>
   );
 };
