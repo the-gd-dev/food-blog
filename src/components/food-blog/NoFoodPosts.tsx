c31bab5c7fd8f85eaae068612ccebc51b6688fa1
Ghanshyam Dutt <ghanshyamdutt1407@gmail.com>
2025-04-11 23:57:12 +0530

    No Posts Create Post Conditional Rendering.

--- src/components/food-blog/NoFoodPosts.tsx
+++ src/components/food-blog/NoFoodPosts.tsx
@@ -3,14 +3,14 @@ import React from "react";
 import { Button } from "@/components";
 
 export const NoFoodPosts = () => {
-  const { toggleCreatePost } = useStore();
+  const { toggleCreatePost, isAuthenticated } = useStore();
   return (
     <div className="rounded-xl h-50 lg:h-90 mt-3 w-full flex flex-col items-center justify-center bg-gray-100 shadow-md p-4">
       <h1 className="text-xl font-medium text-gray-800">No New Posts Found</h1>
       <p className="text-sm  font-light mt-2 text-gray-600 text-center mb-3">
         It looks like there are no posts yet. Why not create one?
       </p>
-      <Button onClick={toggleCreatePost}>Create A New Post</Button>
+      {isAuthenticated && <Button onClick={toggleCreatePost}>Create A New Post</Button>}
     </div>
   );
 };
